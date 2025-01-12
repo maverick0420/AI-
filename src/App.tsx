3298d1ca78046b82f1244d7bf8b0d1b391960d48
Shawn Wang <wangxiao9244@hotmail.com>
2025-01-12 21:24:42 +0800

    Fix React type declarations and import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthProvider } from './hooks/useAuth';
+import { AuthProvider } from './hooks';
 import Layout from './components/Layout';
 import Router from './router';
 
